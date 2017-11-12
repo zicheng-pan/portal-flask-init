66164dbe2b1975e98356f2e6e993ec90bde73ab9
zicheng.pan <1094275633@qq.com>
2017-11-12 19:09:14 +0800

    try my best to use git

---
+++ index.py
@@ -0,0 +1,88 @@
+from flask import Flask,request
+from flask import render_template,make_response
+import json
+import os
+app = Flask(__name__)
+json_location = "json/data.json"
+
+def loadJsonFile():
+    if os.path.exists(json_location):
+        with open(json_location, 'rb') as f:
+            rep = f.read()
+            return rep
+    else:
+        open(json_location, 'w').close()
+        return "[]"
+
+@app.route("/")
+def index():
+    return render_template('portal.html')
+
+@app.route("/left")
+def left():
+    return render_template('left.html')
+
+@app.route("/right")
+def right():
+    return render_template('right.html')
+
+
+@app.route("/addjson")
+def addjson():
+    data = request.args.get("data")
+    result = json.loads(data)
+    all_data = json.loads(loadJsonFile())
+    if result["parentNode"].strip() and result["parentNode"].strip() in [child["name"].strip() for child in all_data] and (result["name"] != "" or result["url"] != ""):
+        for item in all_data:
+            if result["parentNode"].strip() == item["name"]:
+                flag = True
+                for sub in item["subContent"]:
+                    if sub["name"] == result["name"] and result["url"] == "":
+                        item["subContent"].remove(sub)
+                        flag = False
+                    elif sub["name"] == result["name"] and result["url"] != "":
+                        sub["url"] = result["url"]
+                        flag = False
+                if flag:
+                    del result["parentNode"]
+                    item["subContent"].append(result)
+
+    elif result["parentNode"].strip() and result["name"].strip() == "" and result["url"].strip() == "" :
+        for item in all_data:
+            if item["name"] == result["parentNode"]:
+                all_data.remove(item)
+
+    else:
+        del result["parentNode"]
+        result["subContent"] = []
+        all_data.append(result)
+
+    with open(json_location,"wb") as f:
+        json.dump(all_data,f)
+
+    return "success"
+
+@app.route("/getjson")
+def getjson():
+    if os.path.exists(json_location):
+        with open(json_location, 'rb') as f:
+            rep = f.read()
+            return rep
+    else:
+        return "[]"
+
+@app.route("/login")
+def login():
+    info = request.args.get("info","null")
+    user = "null"
+    if info and info != "null":
+        if info.split('/')[0] == "zerg" and info.split('/')[1] == "zerg":
+            user = "zerg"
+        else:
+            user = "fail"
+    return render_template('admin_login.html',user=user)
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True,host="0.0.0.0")
